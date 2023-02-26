9a33fc1a85b623909f084893c40a2535934beeb7
ridwanabdulquadri <121479740+ridwanabdulquadri@users.noreply.github.com>
2023-02-26 20:34:55 +0100

    i created class human

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Practice with git hub");
+        Human human = new Human("Ridwan", 50);
+
+        System.out.println(human);
     }
 }
